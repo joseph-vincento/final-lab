88078ee82cfd50721d2b95309d1c9ccbbb388214
joseph-vincento <joseph.vincento@uconn.edu>
2023-12-06 14:24:14 -0500

    adding quiz questions

---
+++ quiz-app/src/questions.js
@@ -0,0 +1,67 @@
+/* 
+  Questions were generated with ChatGPT using the following prompt:
+
+  Generate an array of 10 sci fi related questions using the following format:
+  const question = [
+    {
+      question:"...", 
+      options:["...","...","...","..."],
+      correctAnswer:"..."}, 
+      ....
+  ];
+*/
+
+const questions = [
+  {
+    question: "In which year does the movie 'Blade Runner' take place?",
+    options: ["2019", "2049", "2100", "2202"],
+    correctAnswer: "2019"
+  },
+  {
+    question: "Who is the author of the 'Dune' series?",
+    options: ["Isaac Asimov", "Philip K. Dick", "Arthur C. Clarke", "Frank Herbert"],
+    correctAnswer: "Frank Herbert"
+  },
+  {
+    question: "Which sci-fi film features a computer system named HAL 9000?",
+    options: ["The Matrix", "2001: A Space Odyssey", "Blade Runner", "E.T. the Extra-Terrestrial"],
+    correctAnswer: "2001: A Space Odyssey"
+  },
+  {
+    question: "What is the name of the spaceship in 'Star Trek' commanded by Captain Kirk?",
+    options: ["USS Enterprise", "Millennium Falcon", "Nostromo", "Serenity"],
+    correctAnswer: "USS Enterprise"
+  },
+  {
+    question: "Who wrote the novel 'I, Robot' which inspired the movie of the same name?",
+    options: ["H.G. Wells", "Isaac Asimov", "Ray Bradbury", "Philip K. Dick"],
+    correctAnswer: "Isaac Asimov"
+  },
+  {
+    question: "Which sci-fi TV series follows the adventures of the crew of the Rocinante?",
+    options: ["The Expanse", "Battlestar Galactica", "Firefly", "Doctor Who"],
+    correctAnswer: "The Expanse"
+  },
+  {
+    question: "In 'The Hitchhiker's Guide to the Galaxy', what is the answer to the ultimate question of life, the universe, and everything?",
+    options: ["42", "23", "69", "17"],
+    correctAnswer: "42"
+  },
+  {
+    question: "Who played the role of Neo in 'The Matrix' trilogy?",
+    options: ["Keanu Reeves", "Tom Cruise", "Will Smith", "Brad Pitt"],
+    correctAnswer: "Keanu Reeves"
+  },
+  {
+    question: "Which sci-fi series features a group of humans trying to find a new home, known as Earth, after the destruction of their colonies?",
+    options: ["Stargate SG-1", "Battlestar Galactica", "Farscape", "Lost in Space"],
+    correctAnswer: "Battlestar Galactica"
+  },
+  {
+    question: "What is the name of the artificial intelligence system in '2001: A Space Odyssey'?",
+    options: ["Siri", "Cortana", "Alexa", "HAL 9000"],
+    correctAnswer: "HAL 9000"
+  }
+];
+
+export default questions;
